ec4e6011f36b842a0dfd276789a24477bc072577
ThiagoAndrade92 <tihaguinho92@gmail.com>
2025-03-16 11:22:56 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const menuHambuerger = document.querySelector('.menu-hamburger')
+const line1 = document.querySelector('.line1');
+const line2 = document.querySelector('.line2');
+const line3 = document.querySelector('.line3');
+const navMobile = document.querySelector('.nav-mobile')
+
+menuHambuerger.addEventListener('click', () =>{
+    line1.classList.toggle('active');
+    line2.classList.toggle('active');
+    line3.classList.toggle('active');
+    navMobile.classList.toggle('active')
+})
